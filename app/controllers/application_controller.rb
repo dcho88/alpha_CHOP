aea61e59a74e1da195878aef28a2c8953b8c7b8e
Dave <azndavecho@gmail.com>
2013-06-16 21:37:04 -0400

    Fixed the redirect page after sign-in & sign-out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+   
+  before_filter :authenticate_user!
+
+  def after_sign_in_path_for(user)
+    snps_path
+  end
+  
+  def after_sign_out_path_for(user)
+    home_index_path
+  end
 
 end
